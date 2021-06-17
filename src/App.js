141a4dffa21f24dec4751b4ed78351f993153a28
srd5853 <43919304+srd5853@users.noreply.github.com>
2021-06-17 18:09:05 -0400

    added tasks and bottom addition button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import Header from "./component/header"
 import Task from "./component/Task"
+import IconButton from '@material-ui/core/IconButton';
+import Delete from '@material-ui/icons/Add';
 import './App.css';
 
 function App() {
+
+  var obj1 = {name: "task 1 qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm", date:"05/04/20", status:"complete", isPinned:true}
+  var obj2 = {name: "task 2", date:"01/01/01", status:"complete", isPinned:false}
+  var obj3 = {name: "task 3", date:"99/99/99", status:"incomplete", isPinned:false}
+
+  const listItems = [obj1, obj2, obj3];
+
   return (
     <div className="App">
-    <Header/>
-    <Task/>
+      {listItems.map((item, index) =>
+              <Task number={index} isPinned={item.isPinned} name={item.name} date={item.date} status={item.status}/>
+      )}
+      <div class = "add-bar">
+          <button aria-label="delete">
+            <Delete/>
+            <p> Add a new task</p>
+          </button>
+      </div>
     </div>
   );
 }
--- src/component/Task/task.js
+++ src/component/Task/task.js
@@ -1,14 +1,41 @@
-export default function Task(){
+import './task.css';
+import IconButton from '@material-ui/core/IconButton';
+import Delete from '@material-ui/icons/Delete';
+import Edit from '@material-ui/icons/Edit';
+import Pin from '@material-ui/icons/PinDrop';
+
+
+export default function Task(props){
+    const statusColorList = ["green", "yellow", "red"]
+    let currentColor = 0;
+
+    function changeColor() {
+        if (currentColor === 2){
+            currentColor = 0;
+        } else {
+            currentColor ++;
+        }
+        document.querySelector('#status').lastElementChild.style.backgroundColor = statusColorList[currentColor]
+    }
+
     return (
-        <div class="outer-task-div">
+        <div class={`outer-task-div ${props.isPinned ? "pinned" : ""}`}>
             <div class="info-div">
-                <p class="task-name"> This is the task name </p>
-                <p class="task-date"> This is the date </p>
-                <p class="task-status"> This is the status </p>
+                <p class="task-name"> {props.name} </p>
+                <p class="task-date"> {props.date} </p>
+                <p id="status" class="task-status"> Status <button onClick={changeColor} class="status-bar"></button></p>
+                
             </div>
-            <div>
-                <button> </button>
-                <button> </button>
+            <div class="buttons-div">
+                <IconButton aria-label="delete">
+                    <Edit />
+                </IconButton>
+                <IconButton aria-label="delete">
+                    <Pin />
+                </IconButton>
+                <IconButton aria-label="delete">
+                    <Delete />
+                </IconButton>
             </div>
         </div>
     );
