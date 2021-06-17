58d483519ca1e3b0dbeb37ac8cb8616d0d9b6b1d
srd5853 <43919304+srd5853@users.noreply.github.com>
2021-06-17 08:46:30 -0400

    I added a new task component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./component/header"
+import Task from "./component/Task"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <Task/>
     </div>
   );
 }
---
+++ src/component/Task/task.js
@@ -0,0 +1,15 @@
+export default function Task(){
+    return (
+        <div class="outer-task-div">
+            <div class="info-div">
+                <p class="task-name"> This is the task name </p>
+                <p class="task-date"> This is the date </p>
+                <p class="task-status"> This is the status </p>
+            </div>
+            <div>
+                <button> </button>
+                <button> </button>
+            </div>
+        </div>
+    );
+}
